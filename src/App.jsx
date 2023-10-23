47230becc96c251a807710f4548a8fbb076951a3
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-23 21:14:53 +0000

    imported assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
-
+import cardsBack from './assets/bg-card-back.png'
+import cardsFront from './assets/bg-card-front.png'
+import desktop from './assets/bg-main-desktop.png'
+import mobile from './assets/bg-main-mobile.png'
+import logo from './assets/card-logo.svg'
+import mark from './assets/icon-complete.svg'
 import './App.css'
 
 function App() {
