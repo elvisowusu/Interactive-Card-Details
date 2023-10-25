1d9767cd6b14f138c1fc4f29962d38565f4822c0
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-25 15:49:30 +0000

    added customised colors to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          'Lineargradient(active input border)': 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
+          'Red(input errors)': 'hsl(0, 100%, 66%)',
+          'White': 'hsl(0, 0%, 100%)',
+          'Lightgrayishviolet': 'hsl(270, 3%, 87%)',
+          'Darkgrayishviolet': 'hsl(279, 6%, 55%)',
+          'Verydarkviolet': 'hsl(278, 68%, 11%)',
+      }
+    },
   },
   plugins: [],
 }
