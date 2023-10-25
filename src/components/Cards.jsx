44eb523e90ede744ea708e98a105cc93a327e5de
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-25 13:12:45 +0000

    improvised

---
+++ src/components/Cards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Cards(){
+    return(
+        <div>
+          <div className={`bg-[url('./assets/bg-card-front.png')] h-[15.3rem] w-[27.97rem] rounded-lg`}>
+          
+          </div>
+          <div className={`bg-[url('./assets/bg-card-back.png')] h-[15.3rem] w-[27.97rem] rounded-lg`}>
+  
+          </div>
+        </div>
+    )
+}
