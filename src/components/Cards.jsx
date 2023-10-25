cea28d841b137618babb7fd27fa631b878550b1c
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-25 15:45:45 +0000

    added react icons

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {FaCircle,FaRegCircle} from 'react-icons/fa'
 
 
 export default function Cards(){
     return(
-        <div className="bg-green-600 w-[32rem]">
+        <div className="bg-green-600 w-[35rem] flex flex-col">
           <div className={`bg-[url('./assets/bg-card-front.png')] h-[15.3rem] w-[27.97rem] rounded-lg`}>
-          
+            <div><FaCircle/> <FaRegCircle/></div>
           </div>
           <div className={`bg-[url('./assets/bg-card-back.png')] h-[15.3rem] w-[27.97rem] rounded-lg`}>
   
