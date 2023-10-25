46634ee707b967e29f447b676286878862ed4c35
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-25 16:30:20 +0000

    added more styles

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,13 +7,13 @@ export default function Cards(){
         <div className="bg-green-600 w-[35rem] flex flex-col">
           <div className={`bg-[url('./assets/bg-card-front.png')] h-[15.3rem] w-[27.97rem] rounded-lg flex flex-col justify-between p-[1.65rem]`}>
             <div className="flex items-center gap-3">
-              <FaCircle className="w-[3.1rem] h-[3.1rem]"/> 
-              <FaRegCircle className="w-[1.3rem] h-[1.3rem]"/>
+              <FaCircle className="w-[3.1rem] h-[3.1rem] text-White"/> 
+              <FaRegCircle className="w-[1.3rem] h-[1.3rem] text-Lightgrayishviolet"/>
             </div>
             <div>
-              <p className="text-[1.9rem] mb-[1.1rem]">9591 -6489 -6389 -1011</p>
+              <p className="text-[1.9rem] mb-[1.1rem]">0000 -0000 -0000 -0000</p>
               <div className="flex justify-between text-Lightgrayishviolet mb-[0.2rem]">
-                <p className="text-[]">FELICIA LEIRE</p>
+                <p className="">JANE APPLESEED</p>
                 <p>09/26</p>
               </div>
             </div>
