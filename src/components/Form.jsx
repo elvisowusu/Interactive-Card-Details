34ea5e33a2320b9a0f35273c3d67f51984f5fd6f
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-25 21:00:47 +0000

    almost done with the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,30 +5,29 @@ import mark from '../assets/icon-complete.svg'
 export default function Form(){
     return(
         
-        <form className="mt-[8.88rem] bg-red-300 px-[2.3rem] flex flex-col ">
+        <form className="mt-[8.88rem] bg-red-300 px-[1.2rem] flex flex-col ">
             <div>
-                <label className="tracking-[0.2rem] font-bold text-[1.05rem]" >CARDHOLDER NAME</label>
-                <input className="border block mt-[0.8rem] h-[4.2rem] rounded-xl placeholder:text-[1.6rem] pt-2 px-[2rem]" type="text" placeholder="e.g Jane Appleased"/>
+                <label className="tracking-[0.2rem] font-bold text-[1.05rem] text-Verydarkviolet" >CARDHOLDER NAME</label>
+                <input className="border block mt-[0.8rem] h-[4.2rem] rounded-xl placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] w-[33rem]" type="text" placeholder="e.g Jane Appleased"/>
             </div>
             <div className="mt-[2.05rem]">
-                <label className="tracking-[0.2rem] font-bold text-[1.05rem]">CARD NUMBER</label>
-                <input className="border block mt-[0.8rem] h-[4.25rem] rounded-xl placeholder:text-[1.6rem] pt-2 px-[2rem]" type="text" />
+                <label className="tracking-[0.2rem] font-bold text-[1.05rem] text-Verydarkviolet">CARD NUMBER</label>
+                <input className="border block mt-[0.8rem] h-[4.25rem] rounded-xl placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] w-[33rem]" type="text" placeholder="e.g. 1234 5678 9123 0000"/>
             </div>
             <div className="mt-[2.05rem] flex gap-[1rem]">
                <div>
-                        <label className="tracking-[0.16rem] font-bold text-[1.05rem]">EXP. DATE (MM/YY)</label>
+                        <label className="tracking-[0.16rem] font-bold text-[1.05rem] text-Verydarkviolet">EXP. DATE (MM/YY)</label>
                         <div className="flex gap-3">
-                            <input className="border block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl placeholder:text-[1.6rem] pt-2 px-[2rem]" type="text" placeholder="" />
-                            <input className="border block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl placeholder:text-[1.6rem] pt-2 px-[2rem]" type="text" placeholder="YY"/>
+                            <input className="border block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]" type="text" placeholder="MM" />
+                            <input className="border block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]" type="text" placeholder="YY"/>
                         </div>
                 </div> 
                 <div>
-                    <label className="tracking-[0.16rem] font-bold text-[1.05rem]">CVC</label>
-                    <input className="border block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl placeholder:text-[1.6rem] pt-2 px-[2rem]" type="text" />
+                    <label className="tracking-[0.16rem] font-bold text-[1.05rem] text-Verydarkviolet">CVC</label>
+                    <input className="border block mt-[0.8rem] h-[4.25rem] w-[19rem] rounded-xl placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]" type="text" placeholder="e.g. 123" />
                 </div>
             </div>
-
-            
+            <button className="text-[1.6rem] h-[4.25rem] rounded-xl bg-Verydarkviolet text-white mt-[2.85rem]">Confirm</button>
 
         </form>
         
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       colors:{
-          'Lineargradient(active input border)': 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
+          'activeinputborder': 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
           'Red(input errors)': 'hsl(0, 100%, 66%)',
           'White': 'hsl(0, 0%, 100%)',
           'Lightgrayishviolet': 'hsl(270, 3%, 87%)',
