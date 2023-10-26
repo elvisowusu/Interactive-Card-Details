78fbb54e8a0c05605ab0402b1cf208f531c04423
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-26 08:41:25 +0000

    done with error messages

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,19 +10,25 @@ export default function Form(){
     const schema =yup.object().shape({
         name:yup
         .string()
-        .required(),
+        .required("Can't be blank"),
         Number:yup
         .number()
-        .required(),
+        .typeError("Can't be blank")
+        .required("Can't be blank"),
         MM:yup
         .string()
-        .required(),
+        .required("Can't be blank"),
         YY:yup
+        .number('YY is required')
+        .typeError("Can't be blank")
+        .required("Can't be blank"),
+        cvc:yup
         .number()
-        .required(),
+        .typeError("Can't be blank")
+        .required("Can't be blank")
         
     })
-    const {register,handleSubmit}=useForm({
+    const {register,handleSubmit,formState:{errors}}=useForm({
         resolver: yupResolver(schema)
     });
 
@@ -39,6 +45,7 @@ export default function Form(){
                        placeholder="e.g Jane Appleased"
                        {...register("name")}
                 />
+                <p>{errors.name?.message}</p>
             </div>
             <div className="mt-[2.05rem]">
                 <label className="tracking-[0.2rem] font-bold text-[1.05rem] text-Verydarkviolet">CARD NUMBER</label>
@@ -47,26 +54,34 @@ export default function Form(){
                         placeholder="e.g. 1234 5678 9123 0000"
                         {...register('Number')}
                 />
+                <p>{errors.Number?.message}</p>
             </div>
             <div className="mt-[2.05rem] flex gap-[1rem]">
                <div>
                         <label className="tracking-[0.16rem] font-bold text-[1.05rem] text-Verydarkviolet">EXP. DATE (MM/YY)</label>
                         <div className="flex gap-3">
+                            <div>
                             <input className="border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]"
                                     type="text" 
                                     placeholder="MM" 
                                     {...register('MM')}
                             />
+                            <p>{errors.MM?.message}</p>
+                            </div>
+                            <div>
                             <input className="border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]" 
                                     type="text" 
                                     placeholder="YY"
                                     {...register('YY')}
                             />
+                            <p>{errors.YY?.message}</p>
+                            </div>
                         </div>
                 </div> 
                 <div>
                     <label className="tracking-[0.16rem] font-bold text-[1.05rem] text-Verydarkviolet">CVC</label>
                     <input className="border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet block mt-[0.8rem] h-[4.25rem] w-[19rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]" type="text" placeholder="e.g. 123" />
+                    <p>{errors.cvc?.message}</p>
                 </div>
             </div>
             <button className="text-[1.6rem] h-[4.25rem] rounded-xl bg-Verydarkviolet text-white mt-[2.85rem]">Confirm</button>
