bf08c2d36b8b86f0050593d3e51e5be6788bd858
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-28 22:57:14 +0000

    added more styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -42,17 +42,17 @@ export default function Form({name,updateContent}){
         
         <form onSubmit={handleSubmit(onSubmit)} className="mt-[8.88rem] px-[1.2rem] flex flex-col ">
             <div>
-                <label className="tracking-[0.2rem] font-bold text-[1.05rem] text-Verydarkviolet" >CARDHOLDER NAME</label>
-                <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.name? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] h-[4.2rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] w-[33rem]`}
+                <label className="tracking-[0.2rem] font-bold text-[1.05rem] md:text-[0.6rem] text-Verydarkviolet" >CARDHOLDER NAME</label>
+                <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.name? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] md:mt-[0.2rem] h-[4.2rem] md:h-[3rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] md:placeholder:text-[1rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] w-[33rem] md:px-5 md:pb-2  md:w-[26rem]`}
                        type="text" 
                        placeholder="e.g Jane Appleased"
                        {...register("name")}
                 />
                 <p className="text-inputerrors mt-3 ">{errors.name?.message}</p>
             </div>
-            <div className="mt-[2.05rem]">
-                <label className="tracking-[0.2rem] font-bold text-[1.05rem] text-Verydarkviolet">CARD NUMBER</label>
-                <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.Number? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] h-[4.25rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] w-[33rem]`}
+            <div className="mt-[2.05rem] md:mt-1">
+                <label className="tracking-[0.2rem] font-bold text-[1.05rem] md:text-[0.6rem] text-Verydarkviolet">CARD NUMBER</label>
+                <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.Number? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] md:mt-[0.2rem] h-[4.25rem] md:h-[3rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] md:placeholder:text-[1.2rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] w-[33rem] md:px-5 md:pb-2  md:w-[26rem]`}
                         type="number" 
                         placeholder="e.g. 1234 5678 9123 0000"
                         onInput={()=>{
@@ -71,10 +71,10 @@ export default function Form({name,updateContent}){
             </div>
             <div className="mt-[2.05rem] flex gap-[1rem]">
                <div>
-                        <label className="tracking-[0.16rem] font-bold text-[1.05rem] text-Verydarkviolet">EXP. DATE (MM/YY)</label>
+                        <label className="tracking-[0.16rem] font-bold text-[1.05rem] md:text-[0.6rem] text-Verydarkviolet">EXP. DATE (MM/YY)</label>
                         <div className="flex gap-3">
                             <div>
-                            <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.MM? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]`}
+                            <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.MM? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] md:mt-[0.2rem] h-[4.25rem] md:h-[3rem] w-[6.1rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] md:placeholder:text-[1.5rem] text-center placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] md:px-0 md:pl-2 md:pb-2  md:w-[4.5rem]`}
                                     type="number" 
                                     placeholder="MM" 
                                     onInput={()=>{
@@ -92,7 +92,7 @@ export default function Form({name,updateContent}){
                             <p className="text-inputerrors mt-3">{errors.MM?.message}</p>
                             </div>
                             <div>
-                            <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.YY? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] h-[4.25rem] w-[6.1rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]`}
+                            <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.YY? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] md:mt-[0.2rem] h-[4.25rem] md:h-[3rem] w-[6.1rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] md:placeholder:text-[1.5rem] text-center placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] md:px-3 md:pb-2  md:w-[4.5rem]`}
                                     type="text" 
                                     placeholder="YY"
                                     onInput={()=>{
@@ -112,8 +112,8 @@ export default function Form({name,updateContent}){
                         </div>
                 </div> 
                 <div>
-                    <label className="tracking-[0.16rem] font-bold text-[1.05rem] text-Verydarkviolet">CVC</label>
-                    <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.cvc? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] h-[4.25rem] w-[19rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem]`}
+                    <label className="tracking-[0.16rem] font-bold text-[1.05rem] md:text-[0.6rem] text-Verydarkviolet">CVC</label>
+                    <input className={`border border-Lightgrayishviolet outline-none focus:border-Darkgrayishviolet ${errors.cvc? 'border-inputerrors focus:border-inputerrors':''} block mt-[0.8rem] md:mt-[0.2rem] h-[4.25rem] md:h-[3rem] w-[19rem] rounded-xl text-[1.6rem] placeholder:text-[1.6rem] md:placeholder:text-[1.2rem] placeholder:text-Darkgrayishviolet pt-2 px-[1.5rem] md:px-3 md:pb-3  md:w-[15rem]`}
                             type="text" 
                             placeholder="e.g. 123" 
                             {...register('cvc')}
@@ -121,7 +121,7 @@ export default function Form({name,updateContent}){
                     <p className="text-inputerrors mt-3">{errors.cvc?.message}</p>
                 </div>
             </div>
-            <button className="text-[1.6rem] h-[4.25rem] rounded-xl bg-Verydarkviolet text-white mt-[2.85rem]">Confirm</button>
+            <button className="text-[1.6rem] md:text-[1rem] h-[4.25rem] md:h-[3rem] rounded-xl bg-Verydarkviolet text-white mt-[2.85rem]">Confirm</button>
 
         </form>
         
