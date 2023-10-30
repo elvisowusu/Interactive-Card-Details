7309a827e48d6eba562b7c75e580a402100fabe5
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-30 14:27:54 +0000

    added styles to the h1 and other elements

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -5,9 +5,9 @@ import {Link} from 'react-router-dom'
 
 export default function Completed (){
     return(
-        <div className="mt-[8.88rem] px-[1.2rem]">
-          <div><img src={icon} alt="" /></div> 
-          <h1>THANK YOU </h1>
+        <div className="mt-[8.88rem] px-[1.2rem] text-center">
+          <div className="flex flex-col justify-center items-center mb-[3rem]"><img src={icon} alt="" /></div> 
+          <h1 className="text-Verydarkviolet text-[2.6rem] tracking-[0.4rem]">THANK YOU!</h1>
           <p>We've added your card details</p>
           <Link to='/' className="text-[1.6rem] md:text-[1rem] h-[4.25rem] md:h-[3rem] rounded-md bg-Verydarkviolet text-white mt-[2.85rem] md:mt-[1.9rem] flex justify-center">
              <button>Continue</button>
