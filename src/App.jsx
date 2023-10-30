6cfeca8048aa78a04b2290a73f5b9e2a33d88e12
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-30 13:55:26 +0000

    setting up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useReducer, useState} from 'react'
 import desktop from './assets/bg-main-desktop.png'
 import mobile from './assets/bg-main-mobile.png'
-
-
 import './App.css'
 import Cards from './components/Cards'
 import Form from './components/Form'
 import Completed from './components/Completed'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 const reducer =(state,action)=>{
   switch(action.type){
@@ -39,9 +38,14 @@ function App() {
   
   return (
     <div className='relative font-SpaceGrotesk w-[35.5rem] md:w-full flex flex-col md:flex-row md:h-[100vh] xl:gap-[20rem] lg:gap-[14rem] md:gap-[13rem] md:items-center bg-orange-400 sm:bg-green-500 md:bg-red-500'>
-      <div className={`${state.screenWidth >768?"bg-[url('./assets/bg-main-desktop.png')] bg-cover  h-[100vh] md:w-[21rem] lg:w-[30rem] xl:w-[35rem]":"bg-[url('./assets/bg-main-mobile.png')]"} bg-cover h-[23rem] w-[35.5rem]`}></div>
+      <div className={`${state.screenWidth >768?"bg-[url('./assets/bg-main-desktop.png')] bg-cover  md:h-[100vh] md:w-[21rem] lg:w-[30rem] xl:w-[35rem]":"bg-[url('./assets/bg-main-mobile.png')]"} bg-cover h-[23rem] w-[35.5rem]`}></div>
       <Cards name={state.name} cardNumber={state.cardNumber} expDate={state.expDate} year={state.year} cvc={state.cvc}/>
-      <Form updateContent={updateName}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Form updateContent={updateName}/>}/> 
+          <Route path='/Completed' element={<Completed/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
