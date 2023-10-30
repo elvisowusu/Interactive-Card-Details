afd99a01557784c2f72fa5f24c2cd1e3f787a212
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-30 14:14:41 +0000

    added elements

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -6,7 +6,10 @@ import icon from '../assets/icon-complete.svg'
 export default function Completed (){
     return(
         <div className="mt-[8.88rem] px-[1.2rem]">
-          heheheh  
+          <div><img src={icon} alt="" /></div> 
+          <h1>THANK YOU </h1>
+          <p>We've added your card details</p>
+          <button>Continue</button>
         </div>
     )
 }
