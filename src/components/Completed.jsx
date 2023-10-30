13bf8786a9e8a8d6a073bc7de14f2985271b3b5f
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-30 14:09:41 +0000

    added some styles to the Completed.jsx

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Completed (){
     return(
-        <div>
+        <div className="mt-[8.88rem] px-[1.2rem]">
           heheheh  
         </div>
     )
