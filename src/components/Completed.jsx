81ef920fde1d928b682c96a6fbd35c32c7d5e869
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-30 14:44:15 +0000

    added more styles

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -7,9 +7,9 @@ export default function Completed (){
     return(
         <div className="mt-[8.88rem] px-[1.2rem] mb-[4rem]">
           <div className="flex flex-col justify-center items-center">
-          <img className="mb-[3rem] h-[8rem]" src={icon} alt="" />
-          <h1 className="text-Verydarkviolet text-[2.6rem] tracking-[0.4rem]">THANK YOU!</h1>
-          <p>We've added your card details</p>
+          <img className="mb-[3rem] h-[7rem]" src={icon} alt="" />
+          <h1 className="text-Verydarkviolet text-[2.6rem] tracking-[0.4rem] mb-5">THANK YOU!</h1>
+          <p className="text-[1.8rem] mb-4">We've added your card details</p>
           </div>
           <Link to='/' className="text-[1.6rem] md:text-[1rem] h-[4.25rem] md:h-[3rem] rounded-md bg-Verydarkviolet text-white mt-[2.85rem] md:mt-[1.9rem] flex justify-center">
              <button>Continue</button>
